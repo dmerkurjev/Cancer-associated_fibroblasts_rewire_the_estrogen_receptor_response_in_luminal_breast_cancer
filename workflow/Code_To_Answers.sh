4e6eab5cea55641ad3ea71a9b53266f0b4d589c7
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-09-08 15:36:11 -0700

    Update Code_To_Answers.sh

--- workflow/Code_To_Answers.sh
+++ workflow/Code_To_Answers.sh
@@ -11,8 +11,8 @@ ans_q1 <- unname(sample_lanes["ym"])
 
 # q2: Which sample has the highest read count, T6hrCAFrep1 or T6hrCAFrep2?
 libsize <- colSums(counts)
-if (libsize["T6hrCAFrep1"])> unname(libsize["T6hrCAFrep2"]) print "T6hrCAFrep1"
-if (libsize["T6hrCAFrep2"])> unname(libsize["T6hrCAFrep1"]) print "T6hrCAFrep2"
+if (libsize["T6hrCAFrep1"])> unname(libsize["T6hrCAFrep2"]) ans_q2= "T6hrCAFrep1"
+if (libsize["T6hrCAFrep2"])> unname(libsize["T6hrCAFrep1"]) ans_q2= "T6hrCAFrep2"
 
 # q3: How many genes have counts>5 in sample T0hrCAFrep1?
 ans_q3 <- sum(counts[, "T0hrCAFrep1"] > 5)
